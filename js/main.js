6545f4c76404abc36715090187a59132812160e9
edom18 <edo.m18@gmail.com>
2016-03-16 22:43:27 +0900

    キーイベントで制御できるよう修正

--- js/main.js
+++ js/main.js
@@ -30,12 +30,9 @@
      *
      *  @param camera カメラオブジェクト
      */
-    function PlayerController(character, position, origin) {
-        this.object = new THREE.Object3D();
-        this.character = character;
-        this.object.add(this.character);
-
-        this.init();
+    function PlayerController(character) {
+        this.init(character);
+        this.initEvents();
     }
     PlayerController.prototype = {
 
@@ -44,18 +41,92 @@
         /**
          *  初期化処理
          */
-        init: function () {
-            this.speed = 1;
+        init: function (character) {
+            this.object = new THREE.Object3D();
+            this.character = character;
+            this.object.add(this.character);
+
+            this.speed     = 1;
+            this.direction = 1;
 
             this.origin   = new THREE.Vector3(0, 0, 0);
             this.position = new THREE.Vector3(0, 1, 0);
             this.forward  = new THREE.Vector3(0, 0, 1);
             this.previousForward = new THREE.Vector3(0, 0, 1);
 
+            this.rotateAxis = new THREE.Vector3(0, 1, 0);
+
             this.extractVector = new THREE.Vector4(0, 0, 1, 0);
 
             // 初期姿勢を未設定に
             this.initializedPose = false;
+        },
+
+        /**
+         *  キーボード操作をセットアップ
+         */
+        initEvents: function () {
+            document.addEventListener('keydown', this.handleKeyEvent.bind(this), false);
+        },
+        handleKeyEvent: function (e) {
+            switch(e.keyCode) {
+                case 38: // up
+                    this.direction = 1;
+                    this.move();
+                    break;
+                case 40: // down
+                    this.direction = -1;
+                    this.move();
+                    break;
+                case 37: // left
+                    this.rotate(1);
+                    break;
+                case 39: // right
+                    this.rotate(-1);
+                    break;
+            }
+        },
+
+        /**
+         *  前方ベクトルを取得する
+         */
+        getForward: function () {
+            var forwardVec4 = this.extractVector.clone().applyMatrix4(this.object.matrixWorld);
+            var forward     = new THREE.Vector3(forwardVec4.x, forwardVec4.y, forwardVec4.z).normalize();
+            return forward;
+        },
+
+        /**
+         *  回転させる
+         */
+        rotate: function (dir) {
+            var q   = new THREE.Quaternion();
+            var rad = 1 * Math.PI / 180 * dir;
+            q.setFromAxisAngle(this.rotateAxis, rad);
+
+            var mat = new THREE.Matrix4();
+            var e = mat.elements;
+
+            e[0 * 4 + 0] = 1.0 - 2.0 * q.y * q.y - 2.0 * q.z * q.z;
+            e[0 * 4 + 1] = 2.0 * q.x * q.y + 2.0 * q.w * q.z;
+            e[0 * 4 + 2] = 2.0 * q.x * q.z - 2.0 * q.w * q.y;
+            e[0 * 4 + 3] = 0;
+
+            e[1 * 4 + 0] = 2.0 * q.x * q.y - 2.0 * q.w * q.z;
+            e[1 * 4 + 1] = 1.0 - 2.0 * q.x * q.x - 2.0 * q.z * q.z;
+            e[1 * 4 + 2] = 2.0 * q.y * q.z + 2.0 * q.w * q.x;
+            e[1 * 4 + 3] = 0;
+
+            e[2 * 4 + 0] = 2.0 * q.x * q.z + 2.0 * q.w * q.y;
+            e[2 * 4 + 1] = 2.0 * q.y * q.z - 2.0 * q.w * q.x;
+            e[2 * 4 + 2] = 1.0 - 2.0 * q.x * q.x - 2.0 * q.y * q.y;
+            e[2 * 4 + 3] = 0;
+
+            this.object.matrixAutoUpdate = false;
+            this.object.matrix.multiply(mat);
+            this.object.updateMatrixWorld();
+
+            this.previousForward = this.getForward();
         },
 
         /**
@@ -107,8 +178,7 @@
                 this.initializedPose = true;
             }
 
-            var forwardVec4 = this.extractVector.clone().applyMatrix4(this.object.matrixWorld);
-            var forward     = new THREE.Vector3(forwardVec4.x, forwardVec4.y, forwardVec4.z).setLength(this.speed);
+            var forward = this.getForward().setLength(this.speed * this.direction);
 
             // 進行方向に少しだけ距離を加算
             this.position.sub(forward);
@@ -197,7 +267,7 @@
     light.position.set(1, 1, 1);
     scene.add(light);
 
-    var geometry = new THREE.BoxGeometry(0.02, 0.02, 0.02);
+    var geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
     var material = new THREE.MeshLambertMaterial({
         color: 0x99ccff
     });
@@ -205,7 +275,7 @@
 
     var playerController = new PlayerController(cube);
     playerController.forward  = new THREE.Vector3(1, 0, 0);
-    playerController.position = new THREE.Vector3(0, 0.5, -1);
+    playerController.position = new THREE.Vector3(0, 0.51, -1);
     playerController.origin   = new THREE.Vector3(0, 0, -1);
     playerController.speed = 0.01;
     scene.add(playerController.object);
@@ -222,7 +292,7 @@
         earth.rotation.x += delta * 0.000015;
         earth.rotation.y += delta * 0.000025;
 
-        playerController.move();
+        // playerController.move();
 
         renderer.render(scene, camera);
 
